20543732ceb1c0323e34b5154fddd019f8131861
Miranda <mahmiranda311@gmail.com>
2024-09-06 17:50:35 -0300

    third commit on developt

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@ const URLS_TO_CACHE = [
     'manifest.json',
     'icon.png'
 ];
-//quando e feito pela primeira vez
+//quando e feito pela primeira vez a instalação
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
